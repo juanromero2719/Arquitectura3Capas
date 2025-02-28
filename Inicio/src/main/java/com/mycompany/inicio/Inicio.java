b78c79dbd178ce51f7ba32d7425fb0632d837010
Sebastian Hogar <juanromero2719@gmail.com>
2025-02-27 20:12:22 -0500

    feature: module main

---
+++ Inicio/src/main/java/com/mycompany/inicio/Inicio.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.inicio;
+
+/**
+ *
+ * @author juanr
+ */
+import com.mycompany.vistaescritorio.VistaMain;
+
+public class Inicio {
+
+    public static void main(String[] args) {
+        
+        VistaMain vistaMain = new VistaMain();
+        vistaMain.show();
+        
+    }
+}
